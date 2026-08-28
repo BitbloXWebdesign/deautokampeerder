a79cad3b5b3ebdf4f941642f76c708a8221c5536
BitbloX Admin <info@bitblox.nl>
2026-08-28 09:34:55 +0200

    perf(lighthouse): add media.s-bol.com preconnect hint for 200ms LCP gain as requested by Lighthouse report

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,7 +65,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="nl" className={inter.className}>
       <head>
+        <link rel="preconnect" href="https://media.s-bol.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://images.unsplash.com" crossOrigin="anonymous" />
+        <link rel="dns-prefetch" href="https://media.s-bol.com" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
